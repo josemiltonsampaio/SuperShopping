d52ea893c1d489ecd492e8fdee49ef2745ca9c38
José Milton Sampaio <josemiltondev@gmail.com>
2023-01-27 09:50:25 -0300

    Test Endpoint

---
+++ SuperShopping.ProductAPI/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperShopping.ProductAPI.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Echo([FromBody] string message) => Ok(message);
+
+    [HttpGet] public IActionResult Time() => Ok(DateTime.Now);
+}
--- SuperShopping.ProductAPI/Infrastructure/Exceptions/CategoryInUseDeleteException.cs
+++ SuperShopping.ProductAPI/Infrastructure/Exceptions/CategoryInUseDeleteException.cs
@@ -1,7 +1,7 @@
 ﻿namespace SuperShopping.ProductAPI.Infrastructure.Exceptions;
 public class CategoryInUseDeleteException : EntityInUseDeleteException
 {
-    public CategoryInUseDeleteException(int categoryId) : base($"The category with id :{categoryId} is in use and cannot be deleted.")
+    public CategoryInUseDeleteException(int categoryId) : base($"The category with id:{categoryId} is in use and cannot be deleted.")
     {
     }
 }
